8bf1556331d7d36fd4eaaa51aae5069742b29c52
Alexander de Squilly Nilly Jamestown the Umpteenth Goonlord Foster <7709@balcarras.gloucs.sch.uk>
2025-03-16 20:23:29 +0000

    ENTIRETY OF CYCLE 2- see "sudoku" repository for log

--- main.py
+++ main.py
@@ -110,33 +110,33 @@ def findEmpty(bo):
 
 
 def checkValid(bo, num, pos):
-    # Check row
+    #check row
     for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
+        if bo[pos[0]][i]==num and pos[1]!=i:
             return False
 
-    # Check column
+    #check column
     for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
+        if bo[i][pos[1]]==num and pos[0]!=i:
             return False
 
     # Check 3x3 grid
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
+    box_x=pos[1]//3
+    box_y=pos[0]//3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x*3, box_x*3 + 3):
-            if bo[i][j] == num and (i, j) != pos:
+    for i in range(box_y*3, box_y*3+3):
+        for j in range(box_x*3, box_x*3+3):
+            if bo[i][j]==num and (i, j)!=pos:
                 return False
-
+                
     return True
 
 def solve(bo):
-    find = findEmpty(bo)
+    find=findEmpty(bo)
     if not find:
         return True  # No empty cells left, puzzle solved
     else:
-        row, col = find
+        row, col=find
 
     for i in range(1, 10):
         if checkValid(bo, i, (row, col)):
